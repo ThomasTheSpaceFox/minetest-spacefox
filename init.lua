7e312e1e84c31ad8de6747c6246b2825b0792200
Thomas Leathers <teleathers22@gmail.com>
2015-08-07 04:14:59 -0400

    added files from version 1.0
    this is version 1.0 of the spacefox mod.
    adds foxblock- light emitting node with a fox face on it
    adds magic super beam- a black node with a light "beam circle texture.
    emits light.

---
+++ init.lua
@@ -0,0 +1,14 @@
+minetest.register_node("spacefox:foxblock", {
+  tiles = {"spacefox_foxblock.png"},
+  groups = {snappy=1, choppy=2},
+  light_source = 15,
+  description = "fox block",
+  inventory_image = "spacefox_foxblock.png"
+})
+minetest.register_node("spacefox:superlight", {
+  tiles = {"spacefox_superlight.png"},
+  groups = {snappy=1, choppy=2},
+  light_source = 15,
+  description = "magic super light",
+  inventory_image = "spacefox_superlight.png"
+})
